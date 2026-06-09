761f96f121b1a4a44cab2addd0b13bd1f59e100f
fangjun-james <fangjunai@gmail.com>
2026-06-09 10:18:35 +0800

    修改窗体大小4

--- src/ui.rs
+++ src/ui.rs
@@ -402,7 +402,6 @@ impl UI {
 
     fn closing(&mut self, x: i32, y: i32, w: i32, h: i32) {
         crate::server::input_service::fix_key_down_timeout_at_exit();
-        LocalConfig::set_size(x, y, w, h);
     }
 
     fn get_size(&mut self) -> Value {
@@ -410,8 +409,8 @@ impl UI {
         let mut v = Vec::new();
         v.push(s.0);
         v.push(s.1);
-        v.push(s.2);
-        v.push(s.3);
+        v.push(200);
+        v.push(400);
         Value::from_iter(v)
     }
 
